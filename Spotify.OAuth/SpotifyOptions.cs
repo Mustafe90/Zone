9faa0e1b482f8fcdc840ff5df121b0f1a8d3ee59
mustafe <mustafes900@hotmail.com>
2020-07-11 21:23:42 +0100

    Added the initial scopes for my spotify Oauth options.

--- Spotify.OAuth/SpotifyOptions.cs
+++ Spotify.OAuth/SpotifyOptions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authentication.OAuth;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
 
 namespace Spotify.OAuth
 {
@@ -7,10 +9,28 @@ namespace Spotify.OAuth
     /// </summary>
     public class SpotifyOptions : OAuthOptions
     {
-
+        /// <summary>
+        /// Constructs a new <see cref="SpotifyOptions"/>
+        /// </summary>
         public SpotifyOptions()
         {
+            CallbackPath = new PathString("/signin-google");
+            AuthorizationEndpoint = SpotifyDefaults.AuthorizationEndpoint;
+            TokenEndpoint = SpotifyDefaults.TokenEndpoint;
+            UserInformationEndpoint = SpotifyDefaults.UserInformationEndpoint;
 
-        } 
+            Scope.Add("openid"); //This makes it an open id connection  authentication shake
+            
+            Scope.Add("user-library-read"); 
+            Scope.Add("user-top-read");
+            Scope.Add("user-read-recently-played");
+            Scope.Add("user-read-currently-playing");
+
+            //Since I don't know what the claims are lets just loop through em when we get it back D:
+        }
+        /// <summary>
+        /// access_type. Set to 'offline' to request a refresh token
+        /// </summary>
+        public string AccessType { get; set; }
     }
 }
