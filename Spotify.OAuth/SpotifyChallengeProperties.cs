0af9f6dc2b7597859e6f24d372cc8b64fccdc161
mustafe <mustafes900@hotmail.com>
2020-07-12 00:03:35 +0100

    Added challange properties.

--- Spotify.OAuth/SpotifyChallengeProperties.cs
+++ Spotify.OAuth/SpotifyChallengeProperties.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.AspNetCore.Authentication.OAuth;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 namespace Spotify.OAuth
 {
     public class SpotifyChallengeProperties : OAuthChallengeProperties
     {
+        public SpotifyChallengeProperties() { }
+        public SpotifyChallengeProperties(IDictionary<string, string> items) : base(items)
+        {
+        }
 
+        public SpotifyChallengeProperties(IDictionary<string, string> items, IDictionary<string, object> parameter)
+            : base(items, parameter) { }
+
+        /// <summary>
+        /// If True (false by default) forces user to re-authorize client 
+        /// </summary>
+        public static readonly string ShowDialogKey = "show_dialog";
+
+        public string ShowDialog
+        {
+            get => GetParameter<string>(ShowDialogKey);
+            set => SetParameter(ShowDialogKey, value);
+        }
     }
 }
