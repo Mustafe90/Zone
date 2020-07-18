2f5d92dce7814468adf7dc3f9f4f4d80fce0ec42
mustafe <mustafes900@hotmail.com>
2020-07-19 00:02:47 +0100

    Added few more end point

--- Services/SpotifyHttpClientService.cs
+++ Services/SpotifyHttpClientService.cs
@@ -13,7 +13,7 @@ namespace Zone.Services
         private readonly HttpClient _client;
         private readonly ILogger<SpotifyHttpClientService> _logger;
 
-        public SpotifyHttpClientService(HttpClient client,ILogger<SpotifyHttpClientService> logger)
+        public SpotifyHttpClientService(HttpClient client, ILogger<SpotifyHttpClientService> logger)
         {
             _client = client;
             _logger = logger;
@@ -22,25 +22,92 @@ namespace Zone.Services
         //Todo: Allow user to get next albums since this returns paginated list
         public async Task<AlbumPagination> GetAlbums(string bearerToken)
         {
-            AlbumPagination data = null;
+            AlbumPagination data = new AlbumPagination();
 
             var request = new HttpRequestMessage(HttpMethod.Get, "/v1/me/albums");
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",bearerToken);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
 
             var response = await _client.SendAsync(request);
 
             if (!response.IsSuccessStatusCode)
             {
-                _logger.LogError("Unable to retrieve user albums {0}, {1}",response.StatusCode,response.ReasonPhrase);
+                _logger.LogError("Unable to retrieve user albums {0}, {1}", response.StatusCode, response.ReasonPhrase);
+
+                return data;
             };
             //parse it 
-            var content = await response.Content.ReadAsStringAsync(); 
+            var content = await response.Content.ReadAsStringAsync();
 
             //Todo Write Custom converter    
             data = JsonSerializer.Deserialize<AlbumPagination>(content, new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true,
             });
+
+            return data;
+        }
+
+        public async Task<string> GetTracks(string bearerToken)
+        {
+            var data = string.Empty;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "v1/tracks");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+
+            var response = await _client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.LogError("Unable to get user tracks");
+                return data;
+            }
+
+            //process the data
+            data = await response.Content.ReadAsStringAsync();
+
+            return data;
+        }
+
+        public async Task<string> RecentlyPlayedTracks(string bearerToken)
+        {
+            var data = string.Empty;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/v1/me/player/recently-played");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",bearerToken);
+
+            var response =  await _client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.LogError("Unable to retrieve recently played tracks");
+                return data;
+            }
+
+            data = await response.Content.ReadAsStringAsync();
+
+            //parse it
+
+            return data;
+        }
+
+        public async Task<string> CurrentlyPlayingTrack(string bearerToken)
+        {
+            var data = string.Empty;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "v1/me/player/currently-playing");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+
+            var response = await _client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.LogError("Unable to retrieve user currently playing tracks");
+                return data;
+            }
+
+            data = await response.Content.ReadAsStringAsync();
+
+            //Parse the data
 
             return data;
         }
