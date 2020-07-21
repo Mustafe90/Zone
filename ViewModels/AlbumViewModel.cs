88f1f6867a3633365af119d5e956657cdf8bc644
mustafe <mustafes900@hotmail.com>
2020-07-22 00:18:14 +0100

    slight refactor

--- ViewModels/AlbumViewModel.cs
+++ ViewModels/AlbumViewModel.cs
@@ -4,7 +4,6 @@ namespace Zone.ViewModels
 {
     public class AlbumViewModel : BasicAlbumViewModel
     {
-        public List<ArtistViewModel> Artists { get; set; }
         public List<CopyrightViewModel> Copyrights { get; set; }
         public ExternalIdsViewModel ExternalIdsViewModel { get; set; }
         public List<object> Genres { get; set; }
